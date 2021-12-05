74dc73343ec9ed5b8ef33bb67f14c11dcb9dc77a
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-12-05 14:27:05 +0100

    refactor(vue/Skins): Change scss to css

--- html/src/components/Skins.vue
+++ html/src/components/Skins.vue
@@ -146,107 +146,95 @@
     }
 </script>
 
-<style lang="scss">
-    .clothing-menu-character-container {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-        align-items: center;
-
-        .clothing-menu-option-header {
-            width: 90%;
-            height: 5%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: rgba(255, 50, 40, 0.5);
-            //background: url(../assets/background-red.png);
-            //background-size: 100% 200%;
-            //background-position: center;
-            box-shadow: 0 0 2px #000;
-        }
-
-        .clothing-menu-option-header p {
-            color: white;
-            font-size: 20px;
-            font-family: 'Love Ya Like A Sister', cursive;
-            letter-spacing: 0.2em;
-            margin-top: 3%;
-            filter: drop-shadow(0 0 2px #000);
-        }
-
-        .clothing-menu-option {
-            width: 100%;
-            height: 80%;
-            overflow-y: scroll;
-        }
-
-        .clothing-menu-option-buttons {
-            width: 100%;
-            display: flex;
-            flex-wrap: wrap;
-            flex-direction: row;
-            justify-content: space-evenly;
-            align-items: center;
-
-            .clothing-menu-option-item {
-                width: 90%;
-                margin-left: 2%;
-                margin-top: 10%;
-
-                .clothing-menu-option-item-header {
-                    width: 100%;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    color: white;
-                    font-size: 22px;
-                    font-family: 'Love Ya Like A Sister', cursive;
-                    letter-spacing: 0.2em;
-                }
-
-                .clothing-menu-option-item-wrapper {
-                    height: 100%;
-                    width: 100%;
-                    margin-left: 2%;
-                    display: flex;
-                    justify-content: space-between;
-                    flex-direction: column;
-                    margin-top: 2%;
-
-                    .clothing-menu-option-item-arrows {
-                        width: 100%;
-                        display: flex;
-                        justify-content: space-evenly;
-                        align-items: center;
-                        margin-bottom: 2%;
-                    }
-
-                        .clothing-menu-option-item-left {
-                        padding: 5% 10%;
-                        background-color: rgb(136, 136, 136);
-                        color: #e9e9e9;
-                        text-align: center;
-                        /* line-height: 5vh; */
-                        border-radius: 5px 0px 0px 5px;
-                        transition: all 0.1s linear;
-                    }
-
-                        .clothing-menu-option-item-right {
-                        padding: 5% 10%;
-                        background-color: rgb(136, 136, 136);
-                        color: #e9e9e9;
-                        text-align: center;
-                        /* line-height: 5vh; */
-                        border-radius: 0px 5px 5px 0px;
-                        transition: all 0.1s linear;
-                        margin-right: 10%;
-                    }
-
-                }
-            }
-        }
-    }
+<style>
+  .clothing-menu-character-container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+  }
+  .clothing-menu-character-container .clothing-menu-option-header {
+    width: 90%;
+    height: 5%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(255, 50, 40, 0.5);
+    box-shadow: 0 0 2px #000;
+  }
+  .clothing-menu-character-container .clothing-menu-option-header p {
+    color: white;
+    font-size: 20px;
+    font-family: 'Love Ya Like A Sister', cursive;
+    letter-spacing: 0.2em;
+    margin-top: 3%;
+    filter: drop-shadow(0 0 2px #000);
+  }
+  .clothing-menu-character-container .clothing-menu-option {
+    width: 100%;
+    height: 80%;
+    overflow-y: scroll;
+  }
+  .clothing-menu-character-container .clothing-menu-option-buttons {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+  .clothing-menu-character-container .clothing-menu-option-buttons .clothing-menu-option-item {
+    width: 90%;
+    margin-left: 2%;
+    margin-top: 10%;
+  }
+  .clothing-menu-character-container .clothing-menu-option-buttons .clothing-menu-option-item .clothing-menu-option-item-header {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 22px;
+    font-family: 'Love Ya Like A Sister', cursive;
+    letter-spacing: 0.2em;
+  }
+  .clothing-menu-character-container .clothing-menu-option-buttons .clothing-menu-option-item .clothing-menu-option-item-wrapper {
+    height: 100%;
+    width: 100%;
+    margin-left: 2%;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    margin-top: 2%;
+  }
+  .clothing-menu-character-container .clothing-menu-option-buttons .clothing-menu-option-item .clothing-menu-option-item-wrapper .clothing-menu-option-item-arrows {
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    margin-bottom: 2%;
+  }
+  .clothing-menu-character-container .clothing-menu-option-buttons .clothing-menu-option-item .clothing-menu-option-item-wrapper .clothing-menu-option-item-left {
+    padding: 5% 10%;
+    background-color: #888;
+    color: #e9e9e9;
+    text-align: center;
+    /* line-height: 5vh;
+    */
+    border-radius: 5px 0px 0px 5px;
+    transition: all 0.1s linear;
+  }
+  .clothing-menu-character-container .clothing-menu-option-buttons .clothing-menu-option-item .clothing-menu-option-item-wrapper .clothing-menu-option-item-right {
+    padding: 5% 10%;
+    background-color: #888;
+    color: #e9e9e9;
+    text-align: center;
+    /* line-height: 5vh;
+    */
+    border-radius: 0px 5px 5px 0px;
+    transition: all 0.1s linear;
+    margin-right: 10%;
+  }
 </style>
